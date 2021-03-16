edd0e4bc45c399c146de63374a2540316a6092cd
artem <a.zzhdanov@gmail.com>
2021-03-15 22:28:37 -0400

    Added style to edit pages

--- Controllers/CTypesController.cs
+++ Controllers/CTypesController.cs
@@ -46,107 +46,107 @@ namespace Hager_Ind_CRM.Controllers
         //}
 
         // GET: CTypes/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
 
         // POST: CTypes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,OrderID")] CType @type)
-        {
-            if (ModelState.IsValid)
-            {
-                @type.OrderID = _context.CompanyTypes.Count() + 1;
-                _context.Add(@type);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(@type);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("ID,Name,OrderID")] CType @type)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        @type.OrderID = _context.CompanyTypes.Count() + 1;
+        //        _context.Add(@type);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(@type);
+        //}
 
         // GET: CTypes/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var @type = await _context.Types.FindAsync(id);
-            if (@type == null)
-            {
-                return NotFound();
-            }
-            return View(@type);
-        }
+        //    var @type = await _context.Types.FindAsync(id);
+        //    if (@type == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(@type);
+        //}
 
         // POST: CTypes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,OrderID")] Models.CType @ctype)
-        {
-            if (id != @ctype.ID)
-            {
-                return NotFound();
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("ID,Name,OrderID")] Models.CType @ctype)
+        //{
+        //    if (id != @ctype.ID)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(@ctype);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TypeExists(@ctype.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(@ctype);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(@ctype);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!TypeExists(@ctype.ID))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(@ctype);
+        //}
 
         // GET: CTypes/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var @type = await _context.Types
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (@type == null)
-            {
-                return NotFound();
-            }
+        //    var @type = await _context.Types
+        //        .FirstOrDefaultAsync(m => m.ID == id);
+        //    if (@type == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(@type);
-        }
+        //    return View(@type);
+        //}
 
         // POST: CTypes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var @type = await _context.Types.FindAsync(id);
-            _context.Types.Remove(@type);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var @type = await _context.Types.FindAsync(id);
+        //    _context.Types.Remove(@type);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool TypeExists(int id)
         {
